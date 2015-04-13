9fcb5804df872b9865a67cbcc2f7b2f7848188d6
cholet852 <cholet25@gmail.com>
2015-04-13 22:07:12 +0200

    WhiteRabbit V1.0

---
+++ Nav_WhiteRabbit/Nav_WhiteRabbit/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Nav_WhiteRabbit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.buttonHome);
+            this.panel2.Controls.Add(this.buttonStop);
+            this.panel2.Controls.Add(this.buttonRefresh);
+            this.panel2.Controls.Add(this.buttonForward);
+            this.panel2.Controls.Add(this.buttonBack);
+            this.panel2.Controls.Add(this.buttonGo);
+            this.panel2.Controls.Add(this.textBoxAddress);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(804, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHome.FlatAppearance.BorderSize = 0;
+            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHome.Image = global::Nav_WhiteRabbit.Properties.Resources.gohome_icone_4047_32;
+            this.buttonHome.Location = new System.Drawing.Point(627, 4);
+            this.buttonHome.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(32, 32);
+            this.buttonHome.TabIndex = 6;
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.FlatAppearance.BorderSize = 0;
+            this.buttonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStop.Image = global::Nav_WhiteRabbit.Properties.Resources.stop_icone_9406_32;
+            this.buttonStop.Location = new System.Drawing.Point(669, 4);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(32, 32);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.FlatAppearance.BorderSize = 0;
+            this.buttonRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRefresh.Image = global::Nav_WhiteRabbit.Properties.Resources.refresh_icone_8026_32;
+            this.buttonRefresh.Location = new System.Drawing.Point(92, 4);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(35, 32);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.FlatAppearance.BorderSize = 0;
+            this.buttonForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonForward.Image = global::Nav_WhiteRabbit.Properties.Resources.blue_arrow_in_front_right_side_icone_8631_32;
+            this.buttonForward.Location = new System.Drawing.Point(52, 4);
+            this.buttonForward.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(33, 32);
+            this.buttonForward.TabIndex = 3;
+            this.buttonForward.UseVisualStyleBackColor = true;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.FlatAppearance.BorderSize = 0;
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBack.Image = global::Nav_WhiteRabbit.Properties.Resources.blue_arrow_left_behind_previous_icone_6270_32;
+            this.buttonBack.Location = new System.Drawing.Point(9, 4);
+            this.buttonBack.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonBack.Size = new System.Drawing.Size(32, 32);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGo.FlatAppearance.BorderSize = 0;
+            this.buttonGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGo.Image = global::Nav_WhiteRabbit.Properties.Resources.search_icone_8523_32;
+            this.buttonGo.Location = new System.Drawing.Point(582, 4);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(32, 32);
+            this.buttonGo.TabIndex = 1;
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.Location = new System.Drawing.Point(136, 10);
+            this.textBoxAddress.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(442, 20);
+            this.textBoxAddress.TabIndex = 0;
+            this.textBoxAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxAddress_KeyDown);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.progressBar1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel3.Location = new System.Drawing.Point(0, 389);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(804, 42);
+            this.panel3.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(680, 10);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(115, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(611, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 39);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(804, 354);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(713, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(751, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 32);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(804, 431);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Nav_WhiteRabbit/Nav_WhiteRabbit/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nav_WhiteRabbit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WebBrowser browser = new WebBrowser();
+        int i = 0;
+
+
+        //Initialisation au démarrage du navigateur
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Creation du navigateur, suppression des erreurs de scripts, style centrer, visible, adresse google
+            browser = new WebBrowser();
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.Visible = true;
+            browser.DocumentCompleted += browser_DocumentCompleted;
+            browser.Navigate("http://www.google.fr");
+
+            //Gestion des onglets, ancrage au 4 coins, onglet vide nommé "New Tab", incremente nombre onglet avec i
+            tabControl1.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Right & AnchorStyles.Left;
+            tabControl1.TabPages.Add("New Tab");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(browser);
+            i += 1;
+
+            //Affiche Google par defaut dans la barre d'adresse et valide
+            textBoxAddress.Text = "http://www.google.fr";
+            buttonGo_Click(null,null);
+
+            //Les boutons precedent, suivant et stop sont grisé au demarage
+           // buttonBack.Enabled = false;
+           // buttonForward.Enabled = false;
+           // buttonStop.Enabled = false;
+
+        }
+
+
+        //Quand le chargement est fini
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+
+            textBoxAddress.Text = browser.Url.AbsoluteUri;
+
+            label1.Text = browser.StatusText;
+            buttonStop.Enabled = false;
+            textBoxAddress.Text = browser.Url.ToString();
+            label1.Text = "Loaded";
+            progressBar1.Value = 0;
+        }
+
+
+        //Quand le chargement commence
+        private void Navigate(string address)
+        {
+
+            if (string.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://") && !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+            try
+            {
+                browser.Navigate(new Uri(address));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+
+            buttonStop.Enabled = true; //On active le bouton stop
+
+            label1.Text = browser.StatusText; //On met le status à jour
+
+            //Si il ya une page suivante alors on active le bouton suivant
+            if (browser.CanGoForward)
+            {
+                buttonForward.Enabled = true;
+            }
+            else
+            {
+                buttonForward.Enabled = false;
+            }
+
+            //Si il ya une page precedente alors on active le bouton precedent
+            if (browser.CanGoBack)
+            {
+                buttonBack.Enabled = true;
+            }
+            else
+            {
+                buttonBack.Enabled = false;
+            }
+
+            label1.Text = "Loading...";
+            while (true)
+            {
+                if (progressBar1.Value >= 100)
+                    break;
+                progressBar1.Value += 5;
+            }
+        }
+
+
+//Boutons de Navigation
+
+        //Bouton de recherche
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(textBoxAddress.Text);
+        }
+
+
+        //Bouton precedent
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoBack();
+        }
+
+
+        //Bouton suivant
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoForward();
+        }
+
+
+        //Bouton actualiser
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Refresh();
+        }
+
+
+        //Bouton stop
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonStop.Enabled = false;
+            browser.Stop();
+            label1.Text = "Stopped";
+            progressBar1.Value = 0;
+        }
+
+
+        //Bouton home
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate("http://www.google.com");
+        }
+
+        private void textBoxAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(textBoxAddress.Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser = new WebBrowser();
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.Visible = true;
+            browser.DocumentCompleted += browser_DocumentCompleted;
+            browser.Navigate("http://www.google.fr");
+            tabControl1.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Right & AnchorStyles.Left;
+            tabControl1.TabPages.Add("New Tab");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(browser);
+            i += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.TabPages.Count - 1 > 0)
+            {
+                tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+                tabControl1.SelectTab(tabControl1.TabPages.Count - 1);
+                i -= 1;
+            }
+        }
+
+
+    }
+}
